06c1bd7b31ececbb501c5d468c8aa51950423356
Reza Fabryan <123403871+Nifleheim@users.noreply.github.com>
2023-09-14 11:43:24 +0700

    Create Backup.sh

---
+++ Backup.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# PostgreSQL settings
+PG_USER="your_postgresql_user"
+PG_DB="your_database_name"
+
+# Timestamp for the backup file
+TIMESTAMP=$(date +"%Y%m%d%H%M%S")
+
+# Backup directory (adjust to your preference)
+BACKUP_DIR="/path/to/backup/directory"
+
+# Create a backup file
+BACKUP_FILE="$BACKUP_DIR/$PG_DB-$TIMESTAMP.sql"
+
+# Perform the backup
+pg_dump -U $PG_USER $PG_DB > $BACKUP_FILE
+
+# Check if the backup was successful
+if [ $? -eq 0 ]; then
+    echo "PostgreSQL backup completed successfully: $BACKUP_FILE"
+else
+    echo "PostgreSQL backup failed"
+    exit 1
+fi
+
+
+#chmod +x backup_script.sh
+#./backup_script.sh
+#aws s3 cp $BACKUP_FILE s3://your_s3_bucket/
+#0 0 * * * /path/to/backup_script.sh
